859ed2940d1a23760258920dbabef95d0b57dd75
Ann-Fox <fx8565@mail.ru>
2023-08-13 13:00:41 +0500

    JS ECMA_hw-1

--- JS_ECMA_homework_1/script.js
+++ JS_ECMA_homework_1/script.js
@@ -3,10 +3,10 @@
 // Домашнее задание
 
 // 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора, найти минимальное число в массиве, решение задание должно состоять из одной строки
-// console.log(`task 1`);
+console.log(`task 1`);
 
-// const arr = [1, 5, 7, 9, -8];
-// console.log(Math.min(...arr));
+const arr = [1, 5, 7, 9, -8];
+console.log(Math.min(...arr));
 
 // 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
 console.log(`task 2`);
@@ -14,7 +14,7 @@ console.log(`task 2`);
 function createCounter(initialValue) {
     let num = initialValue;
     return {
-        increment()  {
+        increment() {
             num++;
         },
         decrement() {
@@ -34,10 +34,31 @@ calcCounter.decrement(); //8
 
 console.log(calcCounter.getValue());
 
+
 // 3) Напишите рекурсивную функцию findElementByClass, которая принимает корневой элемент дерева DOM и название класса в качестве аргументов и возвращает первый найденный элемент с указанным классом в этом дереве.
-console.log(`task 2`);
+// Пример
+// const rootElement = document.getElementById('root');
+// const targetElement = findElementByClass(rootElement, 'my-class');
+// console.log(targetElement)
+
+function findElementByClass(rootElement, className) {
+    if (rootElement.className === className) {
+        return rootElement;
+    }
+
+    for (const element of rootElement.children) {
+        let findElement = findElementByClass(element, className);
+        if (findElement !== null) {
+            return findElement;
+        }
+    }
+
+    return null;
+}
+console.log(`task 3`);
 
 // Пример
+
 const rootElement = document.getElementById('root');
 const targetElement = findElementByClass(rootElement, 'my-class');
 console.log(targetElement)
