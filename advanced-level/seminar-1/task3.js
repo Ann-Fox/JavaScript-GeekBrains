6199ee914659d41dcf47a2e417fa29e2d4095751
Ann-Fox <fx8565@mail.ru>
2023-12-24 17:27:34 +0500

    add seminar 1 (advacce-level)

---
+++ advanced-level/seminar-1/task3.js
@@ -0,0 +1,30 @@
+/* <aside>
+ **Текст задания:**
+
+Часто при работе с DOM мы сталкиваемся с коллекциями элементов, которые не являются стандартными массивами, но похожи на них. Однако у таких коллекций нет методов массива, и здесь на помощь приходит Array.from. В этом задании вы научитесь конвертировать коллекции DOM-элементов в массивы и работать с ними.
+
+Дан код html:
+
+```html
+<div>Element 1</div>
+<div data-active="true">Element 2</div>
+<div>Element 3</div>
+<div data-active="true">Element 4</div>
+```
+
+Напишите функцию, которая собирает все элементы <div> на странице, преобразует их в массив и фильтрует только те из них, у которых есть атрибут data-active.
+
+Выведите результат на консоль.
+
+</aside> */
+
+const arryOfDataActive = document.querySelectorAll('div');
+
+const array = Array.from(arryOfDataActive);
+console.log(array)
+
+const filterArray = array.fill((element) => element.hasAttribute('data-active'))
+
+filterArray.forEach(element => {
+    console.log(element);
+});
