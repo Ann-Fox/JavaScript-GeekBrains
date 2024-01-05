31616e342ffb32dc26bc33461d2bb25d775d9737
Ann-Fox <fx8565@mail.ru>
2024-01-05 11:33:03 +0500

    lesson2 - работа с window

---
+++ advanced-level/lesson-2/window.js
@@ -0,0 +1,8 @@
+// Работа с window
+console.log(window);
+// const newWindow = window.open('https://www.example.com', '_blank'); //открывает новое окно браузера
+// window.close(); //закрывает текущее окно
+// window.resizeTo(800, 600); //Изменяет размер окна на 800х600 пикселей
+// console.log(window.location);
+// window.location.href = 'https://www.example.com'; //Перенаправляет на другую страницу
+// const windowWidth = window.innerWidth; // Получает ширину окна
